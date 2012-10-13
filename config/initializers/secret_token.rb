c7bdb18ef60b66dae24e71d4504719696aeefbeb
Jan Krutisch <jan@krutisch.de>
2012-10-13 12:56:57 +0200

    initial checkin of my demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookrack::Application.config.secret_token = '49563a7e62932c0abf8b1f881fbf0bf313be63358709a6d11f524c099f87883d0abda067b24c71e22a247b806450ea966b2a4ccf32cc375ef65366c1789be46c'
